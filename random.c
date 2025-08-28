9a98cbecebff0e25668ed3c220cfb0d86e7a24ba
crissy <tur54776@temple.edu>
2025-08-28 17:33:08 -0400

    rand_string change 3 (and header file)
    random.c is completed alongside the inclusion of a header file by the same name, required in order for the main file to run a separate C file. Executable made (via Git Bash) in order to run both C files without errors regarding 'undefined reference to rand_string'.

--- random.c
+++ random.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
-#include <stdlib.h>
+#include <stdlib.h> // required in order to use rand()
 #include <time.h>
+#include "random.h"
 
 void rand_string(char* s, size_t size) {
     char alphabet[] = "qwertyuiopasdfghjklzxcvbnm";
     for (size_t i = 0; i < size; i++) {
-        int letter = rand() % (int)(sizeof(alphabet) - 1);
+        int letter = rand() % (int)(sizeof(alphabet) - 1); // subtracting 1 accounts for strings ending with '\0'
         s[i] = alphabet[letter];
     }
 }
-
-/*
-int main() {
-    char alpha[] = "qwertyuiopasdfghjklzxcvbnm";
-    printf("%d", (int)(sizeof(alpha) - 1));
-
-    return 0;
-}
-*/
