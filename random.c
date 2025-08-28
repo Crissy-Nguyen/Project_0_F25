c025cc1c933bcc39b1ce83d95b6a9fffe47f42ef
crissy <tur54776@temple.edu>
2025-08-28 12:18:56 -0400

    random.c created

---
+++ random.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+char* rand_string(char* s, size_t size) {
+    char alpha[] = "qwertyuiopasdfghjklzxcvbnm";
+    for (size_t i = 0; i < size; i++) {
+        
+    }
+
+    return;
+}
+
+int main() {
+    char alpha[] = "wowie";
+    printf("%d", (int)(sizeof(alpha) - 1));
+
+    return 0;
+}
